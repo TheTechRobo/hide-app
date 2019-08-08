df5170aca3db87271591289bed5d0d39f4182235
TtR <52163910+TheTechRobo@users.noreply.github.com>
2019-08-08 15:59:34 -0400

    fix

---
+++ hide/hideUITests/hideUITests.m
@@ -0,0 +1,40 @@
+//
+//  hideUITests.m
+//  hideUITests
+//
+//  Created by Tracey Slater on 2019-01-21.
+//  Copyright © 2019 Tracey Slater. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hideUITests : XCTestCase
+
+@end
+
+@implementation hideUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
---
+++ hide/script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#  script.sh
+#  hide
+#
+#  Created by Ittossarom Ynohtna on 2019-07-17.
+#  Copyright © 2019 Thereisnocopyright. No rights reserved.
+echo You found an Easter Egg! Nice!
