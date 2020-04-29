9917dac8be7130853cde8996a8098f1ad231a35c
TtR <52163910+TheTechRobo@users.noreply.github.com>
2020-04-29 09:13:47 -0400

    Revert "Updated Much"
    This reverts commit 5f95bde2ae3aa6f69efcfe7d8f64eed3a6152e9c.

---
+++ hide/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  hide
+//
+//  Created by Ittossarom Ynohtna on 2019-07-17.
+//  Copyright © 2019 Thereisnocopyright. No rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ hide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hide
+//
+//  Created by Anthony Morassutti on 2019-07-17.
+//  Copyright © 2019 Thereisnocopyright. No rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
